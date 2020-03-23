cf4bdc66149778284416644bc7e7d7a4f45b8323
Jakecw97 <g00349377@gmit.ie>
2020-03-23 21:12:20 +0000

    Part 2 Complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -18,7 +18,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -36,5 +36,5 @@ end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  movies = Movies.all
 end
